fc66bc242a51f948246b2e3ef442f670065e5a18
Justin Miller <adventeurs@gmail.com>
2019-12-31 22:52:52 -0600

    DONE

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -39,7 +39,7 @@ public class SearchController {
         model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
 
-        return "search-results";
+        return "search";
     }
 
 }
